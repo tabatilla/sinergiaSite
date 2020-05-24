c3181a74f1eec554a3c8e1ec1a6b2a2d53573ae9
tabatilla <anthonyvasquezzegarra@gmail.com>
2020-05-23 22:35:17 -0500

    avance mapa footer contacto

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,14 @@
-const fun = (saludo) => {
-  return "Hola " + saludo;
-};
+function initMap() {
+  // The location of Uluru
+  var uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({ position: uluru, map: map });
+}
 
-export { fun };
+alert("Hola Mapa!!!");
+initMap();
